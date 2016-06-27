ca5adff7d456a72809383858f02cad21f2441525
alexey.rysev <alexey.rysev@distate.ru>
2016-06-27 17:51:17 +0300

    update

--- app/app.js
+++ app/app.js
@@ -2,5 +2,29 @@ var App = {
   Models: {},
   Collections: {},
   Routers: {},
-  start
+  start() {
+    _.each(_.values(this.Routers), function(Router) {
+      new Router();
+    })
+
+    App.router = new DefaultRouter();
+    Backbone.history.start();
+  },
+  startSubApplication(SubApplication) {
+    if (this.currentSubapp &&
+        this.currentSubapp instanceof SubApplication) {
+      return this.currentSubapp;
+    }
+
+    if (this.currentSubapp && this.currentSubapp.destroy) {
+      this.currentSubapp.destroy();
+    }
+
+    this.currentSubapp = new SubApplication({
+      region: App.mainRegion
+    });
+
+    return this.currentSubapp;
+  }
+
 }
